f0bc48960a68272558e10308ccdfe4a2e12f02ba
Angel Thakur <athakur@ucsd.edu>
2026-04-26 13:54:48 -0700

    Edited buggy code

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -2,34 +2,24 @@
  * 
  * @param {*} num1, first number to add. 
  * @param {*} num2, second number to add. 
- * @param {*} add, boolean value to tell the function what to do. 
- * @returns The sum of the two numbers if add is true and false otherwise.
+ * @returns The sum of the two numbers
  */
-function sumValues(num1, num2, add) {
-    if (add) {
-        const result = 0;
-
-        result = num1 + num2;
-
-        return result;
-    }
-    else {
-        return !add;
-    }
+function sumValues(num1, num2) {
+    return num1 + num2;
 }
 
 /**
  * 
  * @param {*} prices, an array of the original price.
  * @param {*} discount, a number between 0-1 to represent the discount. 
- * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
+ * @returns An array of each price's new price, after the discount is applied. Or an empty array, if prices array is empty.
  */
 function discountPrices(prices, discount) {
     const discounted = []
     const length = prices.length;
     let discountedPrice = 0
     for(let i = 0; i < length; i++) {
-        discountedPrice += prices[i] * (1 - discount);
+        discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
