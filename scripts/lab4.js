1756e184a4ab815f42a3c044f22891e8a3e245c4
Angel Thakur <athakur@ucsd.edu>
2026-04-26 23:43:35 -0700

    Changes made

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -2,23 +2,36 @@
  * 
  * @param {*} num1, first number to add. 
  * @param {*} num2, second number to add. 
+ * @param {*} add, a boolean value. If true, the two numbers will be added together. If false, the function will return false;
  * @returns The sum of the two numbers
  */
-function sumValues(num1, num2) {
-    return num1 + num2;
+function sumValues(num1, num2, add) {
+    if(typeof(num1) !== "number" || typeof(num2) !== "number") {
+        return false;
+    }
+    if(add === true){
+        return num1 + num2;
+    }
+    return false;
 }
 
 /**
  * 
  * @param {*} prices, an array of the original price.
- * @param {*} discount, a number between 0-1 to represent the discount. 
- * @returns An array of each price's new price, after the discount is applied. Or an empty array, if prices array is empty.
+ * @param {*} discount, a number betwee 0-1 to represent the discount. 
+ * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+    if(typeof(price) !== "array") {
+        return false;
+    }
     const discounted = []
     const length = prices.length;
-    let discountedPrice = 0
+    if(length === 0) {
+        return false;
+    }
     for(let i = 0; i < length; i++) {
+        let discountedPrice = 0
         discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
